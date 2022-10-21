ac4addb713c093f15e7cf9a9b14802086c09e0ec
Matthew <gonzalez.matthew.steven@gmail.com>
2022-10-21 10:28:46 -0400

    walkthrough for 10/21

--- js/Warmups.js
+++ js/Warmups.js
@@ -9,11 +9,34 @@ let potato =
         quantity: "2",
         price: "$7.89"
         }
-
-
-
 function getPrice(obj){
     return obj.price ;
 }
 
 console.log(getPrice(potato));
+
+
+
+
+
+// Warmup 10/21
+
+function getLongestString(arr){
+    let longestStr = "";
+    for (let str of arr){
+        if(str.length > longestStr.length){
+            longestStr = str;
+        }
+    }
+    return longestStr;
+}
+
+
+let colors = ["green","blue","perriwinkle"];
+
+console.log(getLongestString(colors));
+
+
+// const longest = (key, array) => Math.max(...array.map(it => it[key].length));
+//
+// console.log(longest("genre", /*in*/ colors));
