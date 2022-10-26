24f2dc4aeb2338f818068ab5bb2b6acb527f0386
Matthew <gonzalez.matthew.steven@gmail.com>
2022-10-26 11:08:25 -0400

    added 10/26

--- js/Warmups.js
+++ js/Warmups.js
@@ -31,3 +31,63 @@ function getLongestString(arr){
     return longestStr;
 }
 
+//Warmup 10/26
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+function getTallestHamster(arr) {
+    let tallGuy = {heightInMM: 0};
+    // arr.forEach(function(hamster){
+    //     if( hamster.heightInMM > tallGuy.heightInMM){
+    //         tallGuy = hamster;
+    //     }else{
+    //         return tallGuy;
+    //     }
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i].heightInMM > tallGuy.heightInMM) {
+//             tallGuy = arr[i];
+//         } else {
+//         }
+//         return tallGuy;
+//     }
+// }
+    for(let el of arr){
+        if(el.heightInMM > tallGuy.heightInMM){
+            tallGuy = el;
+        }
+    }
+    return tallGuy;
+}
+
+console.log(getTallestHamster(hamsters));
