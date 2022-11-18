2eb24a36f8c455423540266fdc1d288fbd0eca85
Matthew <gonzalez.matthew.steven@gmail.com>
2022-11-18 11:57:48 -0500

    completed mouse event exercise

--- js/jQuery-Exercise.js
+++ js/jQuery-Exercise.js
@@ -1,8 +1,14 @@
 "use strict";
 
-$(function(){
-    let listItems = $("li");
-    listItems.css("font-size" , "20px");
-    let theCollection = $("h1, p, li");
-    alert($('h1').html());
-})
+$('h1').click(function(e) {
+    $(this).css("background-color","lavender");
+});
+$('p').click(function(e) {
+    $(this).css("font-size","18px");
+});
+$('li').hover(function() {
+        $(this).css("color", "red");
+    },
+    function() {
+        $(this).css("color", "black");
+    });
